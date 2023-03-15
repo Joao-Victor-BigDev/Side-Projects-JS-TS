86f7d0357b21ddb592c59cc56f630aa856e5d7a9
Joao-Victor-BigDev <contatobigdev@gmail.com>
2023-03-15 20:53:14 -0300

    Projeto Modelo e Timer

---
+++ Timer-setInterval/assets/js/main.js
@@ -0,0 +1,39 @@
+function retornaHora(segundos) {
+    const data = new Date(segundos * 1000); // aqui transformando em segundos
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'GMT' // aqui coloco pra 00 horas
+    })
+}
+const relogio = document.querySelector('.relogio');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.zerar');
+let segundos = 0;
+let timer;
+
+function iniciaRelogio(){
+    timer = setInterval(function() {
+        segundos++
+        relogio.innerHTML = retornaHora(segundos);
+    }, 1000)
+}
+
+iniciar.addEventListener('click', function(event) {
+    relogio.classList.remove('pausado')
+    relogio.classList.remove('zerou')
+    clearInterval(timer);
+    iniciaRelogio();
+});
+
+pausar.addEventListener('click', function(event) {
+    clearInterval(timer);
+    relogio.classList.add('pausado')
+});
+
+zerar.addEventListener('click', function(event) {
+    relogio.classList.add('zerou')
+    clearInterval(timer);
+    relogio.innerHTML = '00:00:00';
+    segundos = 0;
+});
