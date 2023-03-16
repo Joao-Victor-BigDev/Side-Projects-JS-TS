edf23ddf5431d3588001b51ab2e082f174fa6ff5
Joao-Victor-BigDev <contatobigdev@gmail.com>
2023-03-15 21:02:58 -0300

    mudanca para UTC que tambem deixa 00:00:00

--- Timer-setInterval2/assets/js/main.js
+++ Timer-setInterval2/assets/js/main.js
@@ -2,13 +2,10 @@ function retornaHora(segundos) {
     const data = new Date(segundos * 1000); // aqui transformando em segundos
     return data.toLocaleTimeString('pt-BR', {
         hour12: false,
-        timeZone: 'GMT' // aqui coloco pra 00 horas
+        timeZone: 'UTC' // aqui coloco pra 00 horas
     })
 }
 const relogio = document.querySelector('.relogio');
-const iniciar = document.querySelector('.iniciar');
-const pausar = document.querySelector('.pausar');
-const zerar = document.querySelector('.zerar');
 let segundos = 0;
 let timer;
 
